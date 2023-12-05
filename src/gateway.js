471e415037dfe7ac52163d4aa09ae46b1715cb93
Abhijit <128697608+AbhijitGhate1995@users.noreply.github.com>
2023-12-05 19:41:09 +0530

    Changes In media screen

--- src/gateway.js
+++ src/gateway.js
@@ -65,7 +65,7 @@ const GateWayData = () => {
   return (
     <div className="gateway-bg pb-20">
       <div className="xl:flex flex-wrap justify-center bg-[#09124240] krubFont">
-        <div className="flex justify-end items-center">
+        <div className="flex justify-end items-center Menu-class">
           <Menubar
             model={items}
             className={`px-4 py-4 bg-[#1F2A69] rounded-lg text-white ${
