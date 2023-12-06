9b7811725fd7dd64c99223cad4f8522bdf13e51a
Abhijit <128697608+AbhijitGhate1995@users.noreply.github.com>
2023-12-06 20:07:03 +0530

    change log page

--- src/changelog/index.js
+++ src/changelog/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import HeaderData from '../component/header'
 import LogHeader from './logheader'
 import FooterData from '../component/footer'
+import Version from './version'
 
 const ChangeLog = () => {
   return (
     <div>
         <HeaderData/>
         <LogHeader/>
+        <Version/>
         <FooterData/>
     </div>
   )
